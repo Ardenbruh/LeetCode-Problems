19393d3b4c44b1223171b5cd45e244d58eda5946
arden <68152592+Ardenbruh@users.noreply.github.com>
2025-01-27 00:50:30 +0530

    Time: 2 ms (94.25%), Space: 17.9 MB (30.11%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,6 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        return n > 0 and 1162261467 % n == 0
+        return n > 0 and 1162261467 % n == 0 
+        #1162261467 is 3^19, the largest power of 3 that fits in a 32-bit integer.
 
 
              
