5dabc70186cb69bec209f585285f7bdf0c446e0d
arden <68152592+Ardenbruh@users.noreply.github.com>
2025-01-30 00:23:25 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.65%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -2,8 +2,8 @@ class Solution:
     def repeatedCharacter(self, s: str) -> str:
         lis = []
         for i in s:
-            if i in lis:
-                return i
-            else:
+            if i not in lis:
                 lis.append(i)
+            else:
+                return i
         
