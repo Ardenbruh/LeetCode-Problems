d5e03aafdd57d358d73efe8216fa3e0215a9cbc2
arden <68152592+Ardenbruh@users.noreply.github.com>
2025-01-30 01:26:47 +0530

    Time: 0 ms (100%), Space: 17.7 MB (54.74%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -1,11 +1,7 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        seen = 0  
-        for char in s:
-            
-            bit = ord(char) - ord('a')
-           
-            if seen & (1 << bit):
-                return char
-            seen |= (1 << bit)
-        return ''
+        NewSet = set()
+        for el in s:
+            if el in NewSet:
+                return el
+            NewSet.add(el)
