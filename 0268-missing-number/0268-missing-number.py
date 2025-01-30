2cdd7b4fc99565e02e45242473b9c67128298bb3
arden <68152592+Ardenbruh@users.noreply.github.com>
2025-01-30 18:03:45 +0530

    Time: 0 ms (100%), Space: 18.6 MB (57.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        sum = n*(n+1)/2 # this gives 2.0 if u dont type cast the int in the last as its floating point division
+        sum = n*(n+1)//2
 
-        for i in range(len(nums)):
-            sum = sum - nums[i]
-        
-        return int(sum)
+        for i in nums:
+            sum -= i
+        return sum
         
