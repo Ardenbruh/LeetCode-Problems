67a6ca89d0835435aac4aa8cb3a1b4295c0d332b
arden <68152592+Ardenbruh@users.noreply.github.com>
2025-01-31 18:13:05 +0530

    Time: 124 ms (32.22%), Space: 26.8 MB (73.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxprofit = 0
-        minprice = 99999999
+        minprice = 10**5
         for price in prices:
             maxprofit = max(maxprofit,price-minprice)
             minprice = min(minprice, price)
