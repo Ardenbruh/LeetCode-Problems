84a822175852395f4f597637b1e3f626efe182e6
arden <68152592+Ardenbruh@users.noreply.github.com>
2025-02-04 21:45:24 +0530

    Time: 1 ms (32.76%), Space: 17.9 MB (42.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # count = 0
+        # for el in s:
+        #     if count >= 0:
+        #         if el == '(' or '{' or '[':
+        #             count += 1
+        #         else:
+        #             count -= 1
+                
+        # return bool(count)
+        #lol this works for (()()) type questions 
+
+
+        stack = []
+        m = {"{":"}", "(":")", "[":"]"}
+        for el in s:
+            if el in m:
+                stack.append(el)
+            else:
+                if len(stack) == 0:
+                    return False
+                elif m[stack[-1]] == el:
+                    stack.pop()
+                else:
+                    return False
+
+        return True if not stack else False #if not stack = if len(stack) == 0
