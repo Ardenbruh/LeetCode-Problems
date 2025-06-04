297c190e76c4ec7692722d28c3d74c01a6529e93
Vedant Mishra <68152592+Ardenbruh@users.noreply.github.com>
2025-06-04 22:47:07 +0530

    Time: 2 ms (45.16%), Space: 17.9 MB (75.06%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        dic = Counter(nums1)
+        dic = Counter(nums1) #Makes a Occurance Dict
         res = []
         for num in nums2:
-            if dic[num] > 0:
-                res.append(num)
-                dic[num] -= 1
+            if dic[num] > 0: #We check if the occurance is more than 0 (repeating)
+                res.append(num) #Found the repeat so append in list
+                dic[num] -= 1 #Remove the occurance for next checks
         return res
