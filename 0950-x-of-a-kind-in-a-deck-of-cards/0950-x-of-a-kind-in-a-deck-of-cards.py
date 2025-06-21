5631255e1d4870c3d4e8d42936fa69d838abbe15
Vedant Mishra <68152592+Ardenbruh@users.noreply.github.com>
2025-06-21 22:05:27 +0530

    Time: 3 ms (73.03%), Space: 18.4 MB (19.93%) - LeetHub

--- 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.py
+++ 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.py
@@ -6,16 +6,17 @@ class Solution:
             
             return gcd(b, a%b)
 
-        m = {}
-        for i in deck:
-            if i not in m:
-                m[i] = 1
-            else:
-                m[i] += 1
+        # m = {}
+        # for i in deck:
+        #     if i not in m:
+        #         m[i] = 1
+        #     else:
+        #         m[i] += 1
+
+        m = Counter(deck)
 
         g = m[deck[0]]
 
         for count in m.values():
             g = gcd(max(g,count), min(g,count))
-        
         return g>1
