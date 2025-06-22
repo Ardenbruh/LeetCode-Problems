88ed6d3e1f7ead48326479a157b90a20f943482b
Vedant Mishra <68152592+Ardenbruh@users.noreply.github.com>
2025-06-22 23:08:30 +0530

    Time: 0 ms (100%), Space: 18.2 MB (20.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
         m = {n:i for i, n in enumerate(nums1)}
         res = [-1]*len(nums1)
         stack = []
